dfa21e27ca6aafb8fb0188eb73887a9909324b3d
Michał Maj <michalpmaj@gmail.com>
2022-09-11 07:54:08 +0200

    variable assignment

---
+++ 02_arithmetic_operations/04_variable_assignment/Source.cpp
@@ -0,0 +1,37 @@
+/*
+ * As with other programming languages, C++ allows assignment operators.
+ *		Operator	Example		Equivalence
+ *		   +=		 A+=B		   A=A+B
+ *		   -=		 A-=B		   A=A-B
+ *		   *=		 A*=B		   A=A*B
+ *		   /=		 A/=B		   A=A/B
+ *		   %=		 A%=B		   A=A%B
+ */
+
+#include<iostream>
+
+int main()
+{
+	int a = 0;
+	std::cout<<"Variable\t\tOperation\tResult\n";
+	std::cout<<"a = "<<a;
+
+	a += 2;
+	std::cout<<"\t\t\ta += 2 \t\t a = "<<a<<"\n";
+	std::cout<<"a = "<<a<<" : ";
+
+	a -= 4;
+	std::cout<<"\t\ta -= 4 \t\t a = "<<a<<"\n";
+
+	int b = 3;
+	std::cout<<"a = "<<a<<", b = "<<b<<" : ";
+
+	a *= b;
+	std::cout<<"\ta *= b \t\t a = "<<a<<"\n";
+	std::cout<<"a = "<<a<<", b = "<<b<<" : ";
+
+	a /= b;
+	std::cout<<"\ta /= b \t\t a = "<<a<<"\n";
+
+	return 0;
+}
