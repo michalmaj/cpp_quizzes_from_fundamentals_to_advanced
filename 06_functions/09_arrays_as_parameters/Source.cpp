c1e9cee0278a8dfdb17770bdf70b42ad0137273c
Michał Maj <michalpmaj@gmail.com>
2022-09-18 07:48:16 +0200

    arrays_as_parameters

--- 06_functions/09_arrays_as_parameters/Source.cpp
+++ 06_functions/09_arrays_as_parameters/Source.cpp
@@ -1,3 +1,12 @@
+/*
+ * C++ does not allow arrays to be passed to functions, but, as we have seen, it does allow
+ * pointers to be passed.
+ *
+ * There are three methods for passing an array by reference to a function:
+ *  - void functionName(variableType *arrayName)
+ *  - void functionName(variableType arrayName[length of array])
+ *  - void functionName(variableType arrayName[])
+ */
 #include<iostream>
 #include<iomanip>
 
