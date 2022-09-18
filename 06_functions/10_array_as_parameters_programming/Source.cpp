9c822b248b2a668c8685adc732ef1aa8f9228482
Michał Maj <michalpmaj@gmail.com>
2022-09-18 07:52:45 +0200

    arrays as parameters

---
+++ 06_functions/10_array_as_parameters_programming/Source.cpp
@@ -0,0 +1,27 @@
+/*
+ **Goal: create a function that searches an array for
+ **a given value.
+ */
+
+#include<iostream>
+
+int search(int arr[], int size, int key);
+
+int main()
+{
+    const int size = 4;
+    int array[] = { 345,75896,2,543 };
+    int searchKey = 543;
+    std::cout << "Found at: " << search(array, size, searchKey);
+    return 0;
+}
+
+int search(int arr[], int size, int key)
+{
+	for(int i{0}; i < size; ++i)
+	{
+        if (arr[i] == key)
+            return i;
+	}
+    return -1;
+}
