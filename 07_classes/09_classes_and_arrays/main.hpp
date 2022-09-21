d421bea6025371c9c2ce1f1732e4a87d2d41c4fb
Michał Maj <michalpmaj@gmail.com>
2022-09-21 07:13:38 +0200

    classes and arrays

---
+++ 07_classes/09_classes_and_arrays/main.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+/*main.hpp*/
+/*main.hpp*/
+/*header file for main.cpp*/
+#include<iostream>
+
+
+class Student
+{
+    int grade[5];
+    int id;
+public:
+    Student();
+    void setId(int idin);
+    void setGrade(int index, int gradeIn);
+    int getId();
+    int getGrade(int index);
+    int getAvg();
+    void printInfo();
+};
+
+Student::Student()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        grade[i] = 0;
+    }
+    id = 0;
+}
+
+void Student::setId(int idin)
+{
+    id = idin;
+}
+void Student::setGrade(int index, int gradeIn)
+{
+    grade[index] = gradeIn;
+}
+int  Student::getId()
+{
+    return id;
+}
+int  Student::getGrade(int index)
+{
+    return grade[index];
+}
+int  Student::getAvg()
+{
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+        sum = sum + grade[i];
+    return sum / 5.0;
+}
+void Student::printInfo()
+{
+    std::cout << id << "\n";
+    for (int i = 0; i < 5; i++)
+        std::cout << grade[i] << " ";
+}
