47dd78a1e66bd77ec77e5cd0f9584be061e61dd8
Michał Maj <michalpmaj@gmail.com>
2022-09-30 06:58:58 +0200

    overloading

---
+++ 08_overloading/02_overloading_in_a_class/main.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+
+#include<iostream>
+
+class Compare
+{
+public:
+    int findSmaller(int input1, int input2);
+    float findSmaller(float input1, float input2);
+    char findSmaller(char input1, char input2);
+};
+
+inline int Compare::findSmaller(int input1, int input2)
+{
+    if (input1 < input2)
+        return input1;
+    return input2;
+}
+inline float Compare::findSmaller(float input1, float input2)
+{
+    if (input1 < input2)
+        return input1;
+    return input2;
+}
+
+inline char Compare::findSmaller(char input1, char input2)
+{
+    if (input1 < input2)
+        return input1;
+    return input2;
+}
