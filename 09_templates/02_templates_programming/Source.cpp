88e3eb8f6c26809a050c8aaa26be4afcbf422b35
Michał Maj <michalpmaj@gmail.com>
2022-10-02 08:00:20 +0200

    templates

---
+++ 09_templates/02_templates_programming/Source.cpp
@@ -0,0 +1,9 @@
+#include "main.hpp"
+
+int main()
+{
+    int a = 90;
+    int b = 1;
+    std::cout << a << " + " << b << " = " << sumTwo(a, b);
+    return 0;
+}
---
+++ 09_templates/02_templates_programming/main.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+
+template <class T>
+inline T sumTwo(const T& a, const T&b)
+{
+	return a + b;
+}
