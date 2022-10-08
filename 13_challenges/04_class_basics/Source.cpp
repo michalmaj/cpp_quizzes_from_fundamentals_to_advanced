91122c0a1c674c93991be00b4007055532bed9f6
Michał Maj <michalpmaj@gmail.com>
2022-10-08 20:02:08 +0200

    class basics

---
+++ 13_challenges/04_class_basics/Source.cpp
@@ -0,0 +1,16 @@
+#include "Car.h"
+
+int main() {
+    Car car;
+
+    // try to drive 10 times
+    for (int i = 0; i < 10; i++) {
+        bool didDrive = car.drive();
+        if (!didDrive) {
+            // car is broken! must fix it
+            car.fix();
+        }
+    }
+
+    return 0;
+}
